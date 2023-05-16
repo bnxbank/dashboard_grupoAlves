84e7cd514d79c2f81281764f1859f2079468fa00
cristovanlopes <cristovanl1@gmail.com>
2023-05-16 14:39:54 -0300

    adcionado /faturamento fazer ajustes no grafico

--- flaskProject/app.py
+++ flaskProject/app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def dashboard():
-    return render_template('dashboard2.html')
+    return render_template('analisecomercial.html')
+
+@app.route('/faturamento')
+def faturamento():
+    return render_template('faturamento.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
